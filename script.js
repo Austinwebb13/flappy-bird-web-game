dcd21951b6dfbbedc99be17f00b966a30f9ca26d
unknown <austinscottwebb@icloud.com>
2022-06-16 00:26:22 -0400

    draft

--- script.js
+++ script.js
@@ -1,53 +1,63 @@
-var pipe = document.getElementById('pipe');
 var hole = document.getElementById('hole');
-var score = 0;
-var isJumping;
-var spacePressed
+var pipe = document.getElementById('pipe');
+var bird = document.getElementById('bird');
+var jumping = 0;
+var isPaused = false;
 
-function setInitialValues(){
-    gameStopped = false;
-    isJumping = false;
-}
-
+//Giving the hole animation a random variable
 hole.addEventListener('animationiteration', () => {
-    var random = -Math.floor((Math.random()*500)+300);
-    hole.style.top = random + 'px';
+    var random = Math.random()*2;
+    var top = (random*220)+250;
+    hole.style.top = -(top) + 'px';
 });
 
-function setEventListeners() {
 
-document.body.parentElement.addEventListener( 'click', () => {
-    if (gameStopped) return
-    characterJump()
-})
+//pausing the game
 
-document.spacePressed = e => {
-    e = e || window.event
 
-    if ( e.keycode === 32) {
-        if (gameStopped) return
-        characterJump()
+
+
+//getting bird to float down(gravity)
+setInterval(function(){
+    var birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue('top'));
+    if(jumping==0){
+bird.style.top = (birdTop+4)+"px"
+    }
+//hit detection
+    var pipeLeft = parseInt(window.getComputedStyle(pipe).getPropertyValue('left'));
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue('top'));
+    var birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue('top'));
+    var cTop = -(900-birdTop);
+    if((birdTop>880) || ((pipeLeft<40) && (pipeLeft>-50) && ((cTop<holeTop)||
+    (cTop>holeTop+ 170)))) {
+        alert('Game Over.');
+        bird.style.top = 400 + 'px'
+    }
+    if(birdTop> 875){
+        alert('Game Over');
+        bird.style.top =400 + 'px'
+    }
+    if(birdTop< 220){
+        alert('Game Over');
+        bird.style.top =400 + 'px'
+    }
+}, 10); 
+
+//getting the bird to jump up
+function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var birdTop = 
+        parseInt(window.getComputedStyle(bird).getPropertyValue('top'));
+    if((birdTop>6 ) && (jumpCount<15)){
+        bird.style.top = (birdTop-7)+"px";
+    }
+        if (jumpCount>20) {
+            clearInterval(jumpInterval);
+            jumping =0;
+            jumpCount=0;
         }
-    }
-}
-
-function characterJump(){
-   isJumping = true; 
-
-   const jumpInterval = ('setInterval'); () => {
-    chnageGameState({ diff: -3, direction: 'up'})
-
-    if ( jumpCount > 20) {
-
-        clearInterval(  jumpInterval);
-        isJumping = false;
-        jumpCount = 0;
-    }
-
-    jumpCount++}, 10
-   
-}
-
-function chnageGameState(){
-
+        jumpCount++;
+    }, 10);
 }
